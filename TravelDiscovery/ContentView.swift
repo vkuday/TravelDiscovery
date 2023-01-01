487a3dfc01fdd2da9af3b404ff494327225026d6
vkuday <kudayvolkan@gmail.com>
2023-01-01 14:20:21 -0800

    Initial Commit

---
+++ TravelDiscovery/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TravelDiscovery
+//
+//  Created by Volkan Kuday on 1/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by Volkan Kuday on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
